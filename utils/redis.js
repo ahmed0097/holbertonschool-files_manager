ca0e88537d3a6ccff4c1298a5d76411d668b54be
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-04-27 23:59:53 +0100

    redis

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+const redis = require('utils/redis');
+const { promisify } = require('util/redis');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+
+        this.client.on('error', (error) => {
+            console.error(error);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        const getAsync = promisify(this.client.get).bind(this.client);
+        const value = await getAsync(key);
+        return value;
+    }
+
+    async set(key, value, duration) {
+        const setAsync = promisify(this.client.set).bind(this.client);
+        await setAsync(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        const delAsync = promisify(this.client.del).bind(this.client);
+        await delAsync(key);
+    }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;

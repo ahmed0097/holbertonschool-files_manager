89ce357f12c9d9aeab79cea0a40c9e39668c4ad6
ahmed0097 <ahmed.zghal1233@gmail.com>
2023-04-28 16:53:37 +0100

    db

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from ('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(url, { useNewUrlParser: true });
+    this.client.connect();
+    this.db = this.client.db(database);
+  }
+
+  async isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
